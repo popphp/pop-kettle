08a731ed7944d87db91f64c249bcab7a72423e9c
Nick Sagona <nick@noladev.com>
2026-08-14 19:20:15 -0500

    Add Model\Queue::configure() with File adapter support
    Implements Queue model class with configure() method supporting File adapter
    only (Database and Redis adapters to follow in subsequent tasks). Includes
    protected writeEnv() and writeConfig() methods to persist configuration to
    .env and app/config/queue.php files respectively.

---
+++ tests/Model/QueueTest.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Pop\Kettle\Test\Model;
+
+use Pop\Console\Console;
+use Pop\Kettle\Model;
+use Pop\Kettle\Test\Fixtures\AppTestTrait;
+use PHPUnit\Framework\TestCase;
+
+class QueueTest extends TestCase
+{
+
+    use AppTestTrait;
+
+    protected function setUp(): void
+    {
+        $this->enterSandbox();
+        mkdir(getcwd() . '/app/config', 0777, true);
+        copy(__DIR__ . '/../../config/templates/orig.env', getcwd() . '/.env');
+    }
+
+    protected function tearDown(): void
+    {
+        $this->leaveSandbox();
+    }
+
+    public function testInit()
+    {
+        $queue = new Model\Queue();
+        $this->assertInstanceOf('Pop\Kettle\Model\Queue', $queue);
+    }
+
+    public function testConfigureFileDefaultQueue()
+    {
+        $console = new Console(120, '    ');
+        $console->setInputStream($this->createInputStream('1', '', '', '', ''));
+
+        $queue  = new Model\Queue();
+        $result = $queue->configure($console, getcwd());
+
+        $this->assertSame($queue, $result);
+        $this->assertSame('file', $_ENV['QUEUE_ADAPTER']);
+        $this->assertSame('FIFO', $_ENV['QUEUE_PRIORITY']);
+        $this->assertSame('60', $_ENV['QUEUE_LEASE']);
+        $this->assertDirectoryExists(getcwd() . '/database/queue/default');
+        $this->assertFileExists(getcwd() . '/app/config/queue.php');
+
+        $config = include getcwd() . '/app/config/queue.php';
+        $this->assertSame('file', $config['default']['adapter']);
+        $this->assertSame(0, $config['default']['weight']);
+    }
+
+    public function testConfigureFileNamedQueue()
+    {
+        $console = new Console(120, '    ');
+        $console->setInputStream($this->createInputStream('1', '', '', '', '5'));
+
+        $queue = new Model\Queue();
+        $queue->configure($console, getcwd(), 'logging');
+
+        $this->assertSame('file', $_ENV['QUEUE_LOGGING_ADAPTER']);
+        $this->assertDirectoryExists(getcwd() . '/database/queue/logging');
+
+        $config = include getcwd() . '/app/config/queue.php';
+        $this->assertSame('file', $config['logging']['adapter']);
+        $this->assertSame(5, $config['logging']['weight']);
+    }
+
+}
